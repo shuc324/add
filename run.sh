51f30644aaf7dd25fe9dc04b5620e03b8e3baa22
shuc324 <645904877@qq.com>
2017-04-12 17:39:54 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "[mongodb]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/6/mongodb-org/3.2/`uname -m`/\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/mongodb.repo && yum install -y mongodb-org memcached
+echo -e "[mongodb]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/6/mongodb-org/3.2/`uname -m`/\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/mongodb.repo && proxychains4 yum install -y mongodb-org memcached
