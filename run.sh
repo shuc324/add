fc57269d7e65cd937eb26e775ca1a17e9eaa4a89
shuc324 <645904877@qq.com>
2017-04-12 17:47:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm && yum -y install mysql-community-server
+curl -sL https://rpm.nodesource.com/setup_5.x | bash - && yum install -y nodejs
